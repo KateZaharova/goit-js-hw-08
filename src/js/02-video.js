abc1568b8a0bc7f1c02ea9421bc9f5092f475a8b
Kate Zaharova <15071981ktz@gmail.com>
2023-07-02 21:07:50 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,34 @@
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('play', function() {
+        console.log('played the video!');
+    });
+
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+const onPlay = function(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);    
+};
+player.on("timeupdate", throttle(onPlay,1000));
+
+const currentTime = Number(localStorage.getItem("videoplayer-current-time"));
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
